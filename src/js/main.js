ea9917ed88b0be48926cf2dcf8ec327772f0e81a
Des Barrett <des.barrett@gmail.com>
2017-06-21 23:36:47 +0100

    Working responsive app

--- src/js/main.js
+++ src/js/main.js
@@ -1,22 +1,40 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-//setup the standard session of 25 mins
-  var standardSessionMins = 25;
-  $('#set-session').text(standardSessionMins + ':00');
+//setup the standard session of 25 mins and break of 5 mins
+  var standardSessionMins = 5;
+  var breakTime = 1;
+  $('#set-session').text(("0" + standardSessionMins).slice(-2) + ':00');
+  $('#set-break').text(("0" + breakTime).slice(-2) + ':00');
 
-  // adjust time interval between 1 - 59 mins
+  // adjust time interval between 1 - 59 mins for session
     $('#minus-session').click(function(){
       if(standardSessionMins > 1){
         standardSessionMins -= 1;
-        $('#set-session').text(standardSessionMins + ":00");
+        $('#set-session').text(("0" + standardSessionMins).slice(-2) + ":00");
+        $('#current-time').text(("0" + standardSessionMins).slice(-2) + "m" + ":00" + "sec");
       }
     });
     $('#plus-session').click(function(){
       if(standardSessionMins < 59){
         standardSessionMins += 1;
-        $('#set-session').text(standardSessionMins + ":00");
+        $('#set-session').text(("0" + standardSessionMins).slice(-2) + ":00");
+        $('#current-time').text(("0" + standardSessionMins).slice(-2) + "m" + ":00" + "sec");
       }
     });
+
+  // adjust time interval between 1 -15 mins for break
+  $('#minus-break').click(function(){
+    if(breakTime > 1){
+      breakTime -= 1;
+      $('#set-break').text(("0" + breakTime).slice(-2) + ":00");
+    }
+  });
+  $('#plus-break').click(function(){
+    if(breakTime < 15){
+      breakTime += 1;
+      $('#set-break').text(("0" + breakTime).slice(-2) + ":00");
+    }
+  });
     //reset the clock
     $('#reset').click(function(){
        location.reload();
@@ -25,13 +43,21 @@ document.addEventListener('DOMContentLoaded', function () {
     // start the Timer
     $('#start').click(function(){
       startTimer(standardSessionMins);
+
     });
     function startTimer(standardSessionMins) {
       var currentTime = Date.parse(new Date());
       var session = new Date(currentTime + standardSessionMins*60*1000);
+      var barColor = "#4CAF50";
+      var sessionColorOnOff = 0;
+      var sessionColorElement = document.getElementById('sessionText');
+      var clockIcon = document.getElementById('fa-clock');
 
       // Update the count down every 1 second
       var x = setInterval(myTimer, 1000);
+
+      // setup progress bar
+      progressBar(standardSessionMins * 600, barColor);
         // Timer
       function myTimer() {
 
@@ -44,14 +70,95 @@ document.addEventListener('DOMContentLoaded', function () {
           var minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
           var seconds = Math.floor((interval % (1000 * 60)) / 1000);
 
-          // Output the result in an element with id="demo"
-          document.getElementById("current-time").innerHTML = minutes + "m " + ("0" + seconds).slice(-2) + "s";
+          // Output the result to element with id="current-time"
+          document.getElementById("current-time").innerHTML = ("0" + minutes).slice(-2) + "m " + ("0" + seconds).slice(-2) + "s";
+          // flash session text green and white while timing
+          if (sessionColorOnOff == 0) {
+            //document.getElementById("sessionText").style.color = "green";
+            sessionColorElement.style.color = "green";
+            clockIcon.style.color = "green";
+            sessionColorOnOff = 1;
+          } else {
+            document.getElementById("sessionText").style.color = "white";
+            clockIcon.style.color = "white";
+            //sessionColorElement.style.color = '#4CAF50';
+            sessionColorOnOff = 0;
+          }
 
           // When finished write some text
           if (interval < 0) {
+            // clear the timer set with the setInterval() method
               clearInterval(x);
-              document.getElementById("current-time").innerHTML = "Time Up";
+              document.getElementById("current-time").innerHTML = "Time Up Take a Break";
+              window.setTimeout(function(){startBreak(breakTime)},2000);
+              //startBreak(breakTime);
+
           }
+      }
+    }
+    function startBreak(breakTime) {
+      var currentTime = Date.parse(new Date());
+      var session = new Date(currentTime + breakTime*60*1000);
+      var barColor = "#d71919";
+      var sessionColorOnOff = 0;
+      var breakColorElement = document.getElementById('breakText');
+      var clockIcon = document.getElementById('fa-clock');
+
+      // Update the count down every 1 second
+      var x = setInterval(myTimer, 1000);
+
+      // setup progress bar
+      progressBar(breakTime * 600, barColor);
+        // Timer
+      function myTimer() {
+
+          // Get todays date and time
+          var now = new Date().getTime();
+          // Find interval
+          var interval = session - now;
+
+          // Time calculations for minutes and seconds
+          var minutes = Math.floor((interval % (1000 * 60 * 60)) / (1000 * 60));
+          var seconds = Math.floor((interval % (1000 * 60)) / 1000);
+
+          // Output the result in an element with id="current-time"
+          document.getElementById("current-time").innerHTML = minutes + "m " + ("0" + seconds).slice(-2) + "s";
+
+          // flash break text red and white while timing
+          if (sessionColorOnOff == 0) {
+            //document.getElementById("sessionText").style.color = "green";
+            breakColorElement.style.color = "red";
+            clockIcon.style.color = "red";
+            sessionColorOnOff = 1;
+          } else {
+            //document.getElementById("sessionText").style.color = "white";
+            breakColorElement.style.color = "white";
+            clockIcon.style.color = "white";
+            sessionColorOnOff = 0;
+          }
+          // When finished write some text
+          if (interval < 0) {
+            // clear the timer set with the setInterval() method
+              clearInterval(x);
+              document.getElementById("current-time").innerHTML = "Break Time Up";
+              //startTimer(5);
+
+          }
+      }
+    }
+    // progress background
+    function progressBar(setTime, barColor) {
+      var elem = document.getElementById("myBar");
+      var width = 1;
+      var intervalId = setInterval(frame, setTime);
+      function frame() {
+        if (width >= 100) {
+          clearInterval(intervalId);
+        } else {
+          width++;
+          elem.style.width = width + '%';
+          elem.style.backgroundColor = barColor;
+        }
       }
     }
 
